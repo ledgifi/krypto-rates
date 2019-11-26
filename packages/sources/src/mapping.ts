0fd2f8ad8f40e73ef936ea21625535941739d1ca
Sebastian Aranguiz <sarang575@gmail.com>
2019-11-26 17:34:25 -0300

    Move RateSourceByMarket mapping to a yaml config file

--- packages/sources/src/mapping.ts
+++ packages/sources/src/mapping.ts
@@ -1,60 +1,25 @@
+import { workspaceRoot } from '@krypto-rates/common/paths'
+import fs from 'fs'
+import yaml from 'js-yaml'
+import path from 'path'
 import { BitcoinAverageSource } from './clients/bitcoinaverage'
+import { CoinlayerSource } from './clients/coinlayer'
 import { CurrencyLayerSource } from './clients/currencylayer'
 import { RateSource } from './models'
 
-export const RateSourceByMarket = new Map<string, { new (): RateSource }>([
-  // CRYPTO
-  // BCH
-  ['BCH-BTC', BitcoinAverageSource],
-  ['BCH-CLP', BitcoinAverageSource],
-  ['BCH-EUR', BitcoinAverageSource],
-  ['BCH-GBP', BitcoinAverageSource],
-  ['BCH-JPY', BitcoinAverageSource],
-  ['BCH-USD', BitcoinAverageSource],
-  // BTC
-  ['BTC-ARS', BitcoinAverageSource],
-  ['BTC-CLP', BitcoinAverageSource],
-  ['BTC-EUR', BitcoinAverageSource],
-  ['BTC-GBP', BitcoinAverageSource],
-  ['BTC-JPY', BitcoinAverageSource],
-  ['BTC-USD', BitcoinAverageSource],
-  // ETH
-  ['ETH-BTC', BitcoinAverageSource],
-  ['ETH-CLP', BitcoinAverageSource],
-  ['ETH-EUR', BitcoinAverageSource],
-  ['ETH-GBP', BitcoinAverageSource],
-  ['ETH-JPY', BitcoinAverageSource],
-  ['ETH-USD', BitcoinAverageSource],
-  // LTC
-  ['LTC-BTC', BitcoinAverageSource],
-  ['LTC-CLP', BitcoinAverageSource],
-  ['LTC-EUR', BitcoinAverageSource],
-  ['LTC-GBP', BitcoinAverageSource],
-  ['LTC-JPY', BitcoinAverageSource],
-  ['LTC-USD', BitcoinAverageSource],
-  // USDT
-  // ['USDT-USD', FixedRateSource,
-  // XLM
-  ['XLM-BTC', BitcoinAverageSource],
-  // ['XLM-CLP', ComputedRateSource,
-  // ['XLM-USD', ComputedRateSource,
-
-  // FIAT
-  // USD
-  ['USD-ARS', CurrencyLayerSource],
-  ['USD-BRL', CurrencyLayerSource],
-  ['USD-CLP', CurrencyLayerSource],
-  ['USD-COP', CurrencyLayerSource],
-  ['USD-EUR', CurrencyLayerSource],
-  ['USD-GBP', CurrencyLayerSource],
-  ['USD-JPY', CurrencyLayerSource],
-  ['USD-PEN', CurrencyLayerSource],
-  // CLP
-  ['CLP-ARS', CurrencyLayerSource],
-  ['CLP-BRL', CurrencyLayerSource],
-  ['CLP-COP', CurrencyLayerSource],
-  ['CLP-EUR', CurrencyLayerSource],
-  ['CLP-GBP', CurrencyLayerSource],
-  ['CLP-JPY', CurrencyLayerSource],
-  ['CLP-PEN', CurrencyLayerSource],
+export const RateSourceById = new Map<string, { new (): RateSource }>([
+  [BitcoinAverageSource.id, BitcoinAverageSource],
+  [CoinlayerSource.id, CoinlayerSource],
+  [CurrencyLayerSource.id, CurrencyLayerSource],
 ])
+
+export const RateSourceByMarket = new Map<
+  string,
+  { new (): RateSource } | undefined
+>(
+  Object.entries<string>(
+    yaml.safeLoad(
+      fs.readFileSync(path.join(workspaceRoot, 'config/markets.yml'), 'utf8'),
+    ),
+  ).map(([market, name]) => [market, RateSourceById.get(name)]),
+)
