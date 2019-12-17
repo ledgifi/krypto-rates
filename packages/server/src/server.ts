0fd31723037a204db643cfca1ecb3fa31d2dd611
Sebastian Aranguiz <sarang575@gmail.com>
2019-12-17 01:33:17 -0300

    refactor(apollo): enable playground and introspection on production

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -9,6 +9,8 @@ import { schema } from './schema'
 new ApolloServer({
   schema,
   context: createContext,
+  playground: true,
+  introspection: true,
 })
   .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => console.log(`Server ready at ${chalk.cyan(url)}`))
