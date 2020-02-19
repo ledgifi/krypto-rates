c45139bcb1497e73faf27346d2cef0fba09bc2e2
Sebastian Aranguiz <sarang575@gmail.com>
2020-02-19 17:58:34 -0300

    refactor(js-client): export main types on index

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -1,1 +1,12 @@
 export { KryptoRates } from './clients'
+export {
+  Currency,
+  DateInput,
+  FetchBase,
+  Market,
+  MarketDate,
+  MarketTimeframe,
+  Money,
+  Rate,
+  Timeframe,
+} from './types'
