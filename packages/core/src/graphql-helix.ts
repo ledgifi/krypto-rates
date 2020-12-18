f0344a7b4dde96713a2ce11faf34a28e0e7a3bf4
Sebastian Aranguiz <sarang575@gmail.com>
2020-12-18 19:15:51 -0300

    refactor(core): improve error logs

--- packages/core/src/graphql-helix.ts
+++ packages/core/src/graphql-helix.ts
@@ -8,6 +8,8 @@ import {
 import { Context, createContext } from './context'
 import { schema } from './schema'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 export const processRequest = <TRootValue = unknown>(
   options: Omit<
     ProcessRequestOptions<Context, TRootValue>,
@@ -28,9 +30,13 @@ export const formatResult = ({
   errors:
     errors &&
     formatApolloErrors(
-      errors?.map((error) => {
-        console.error(error)
-        return fromGraphQLError(error)
-      }),
+      errors?.map((error) => fromGraphQLError(error)),
+      {
+        debug: isDev,
+        formatter: (error) => {
+          console.error(error.originalError)
+          return error
+        },
+      },
     ),
 })
